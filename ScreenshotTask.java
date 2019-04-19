c68d6db49dc65d181dffd6714632ef8ad5fa61a9
okbabich <ksyukha-babich@yandex.ru>
2019-04-19 18:09:04 +0300

    annotations BeforeClass, AfterClass were added, page refresher is called for BN

--- ScreenshotTask.java
+++ ScreenshotTask.java
@@ -1,5 +1,7 @@
 package skillup;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -7,12 +9,25 @@ import utils.LibraryUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class ScreenshotTask extends Utils {
+import static skillup.Utils.*;
+
+public class ScreenshotTask {
+
+    @BeforeClass
+    public static void setUpClass() {
+        initDriver();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        closeDriver();
+    }
 
     @Test
 
-    public void takeScreenshot() throws InterruptedException {
+    public void takeScreenshot() {
 
         List<Object[]> urls = new ArrayList<>();
 
@@ -23,14 +38,18 @@ public class ScreenshotTask extends Utils {
 
         for (Object[] urlWithName : urls) {
             driver.get(urlWithName[0].toString());
+
+            if(urlWithName[0].equals("https://www.bncollege.com/campus-stores/")){
+                pageRefresher("//*[text()='This site can’t be reached']");
+            }
             WebElement careersText = driver.findElement(By.xpath("//a[contains(text(),'Careers')]"));
             LibraryUtils.waitForElementToBeVisible(driver, careersText, 15);
             ScreenshotTaker.captureScreenshot(driver, urlWithName[1].toString());
+
         }
+
     }
 }
-
-
 
 
 
