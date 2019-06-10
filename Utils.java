1b52fa9a6ad7ec2b11ef8dca3052f52a924658f1
okbabich <ksyukha-babich@yandex.ru>
2019-06-10 16:25:07 +0300

    initRemoteWD, closeRemotWD methods were added

--- Utils.java
+++ Utils.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -17,10 +20,10 @@ import static skillup.Xpathes.*;
 public class Utils {
 
     public static WebDriver driver;
-
+    public static WebDriver driver1;
 
     public static void initDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Chrome Driver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\ChrDriver\\chromedriver.exe");
         //System.setProperty(ChromeDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");       //supress logs with warning
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -28,14 +31,25 @@ public class Utils {
     }
 
     public static WebDriver initTheSameDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Chrome Driver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\ChrDriver\\chromedriver.exe");
         //System.setProperty(ChromeDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");       //supress logs with warning
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        return  driver;
+        return driver;
     }
 
+    public static WebDriver initRemoteWD() throws MalformedURLException {
+        DesiredCapabilities capability = new DesiredCapabilities();
+        capability.setBrowserName("chrome");
+        driver1 = new RemoteWebDriver(new URL("http://10.10.83.231:4444/wd/hub"), capability);
+        return driver1;
+    }
+
+    public static void closeRemotWD() {
+        if (driver1 != null)
+            driver1.quit();
+    }
     public static void closeDriver() {
         if (driver != null)
             driver.quit();
@@ -49,11 +63,12 @@ public class Utils {
             }
         }
     }
+
     public static void getAndCompareIsbn(String xpathGetIsbn, String ISBN13) {
         String IsbnFromThePage = driver.findElement(By.xpath(xpathGetIsbn)).getText()
-                .replace("-","")
-                .replace("ISBN: ","")
-                .replace("ISBN:","");
+                .replace("-", "")
+                .replace("ISBN: ", "")
+                .replace("ISBN:", "");
         assertEquals(ISBN13, IsbnFromThePage);
         System.out.println(IsbnFromThePage);
     }
@@ -61,21 +76,20 @@ public class Utils {
     public static void checkPriceFromThePage(String xpathGetPrice) {
         List<WebElement> buyNewPriceBox = driver.findElements(By.xpath(xpathGetPrice));
 
-        if(driver.findElements(By.xpath(XPATH_CURRENTLY_UNAVAILABLE_AMAZON)).size() > 0 ){
+        if (driver.findElements(By.xpath(XPATH_CURRENTLY_UNAVAILABLE_AMAZON)).size() > 0) {
             System.out.println("Buy New price is absent");
-        }
-        else if(buyNewPriceBox.size() > 0 ) {
+        } else if (buyNewPriceBox.size() > 0) {
             System.out.println("Buy New price exists - " + buyNewPriceBox.get(0).getText());
             assertFalse(buyNewPriceBox.get(0).getText().contains("$"));
-        }
-        else {
+        } else {
             System.out.println("Buy New price is absent");
             assertTrue(true);
         }
     }
-    public static void getAndComparePrice(String xpathGetPrice, String expectedPrice){
+
+    public static void getAndComparePrice(String xpathGetPrice, String expectedPrice) {
         String actualPrice = driver.findElement(By.xpath(xpathGetPrice)).getText()
-                .replace("$","");
+                .replace("$", "");
         assertEquals(expectedPrice, actualPrice);
         System.out.println("Parsed price is matched with website");
     }
